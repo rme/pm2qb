257f0767cb86231767b22e7834a81e6b4d254026
rme <rme.plan.b@gmail.com>
2012-08-09 12:58:45 -0400

    2nd. clean plugin

--- pm2qb/services/example_api_client.php
+++ pm2qb/services/example_api_client.php
@@ -17,9 +17,6 @@ if (function_exists('date_default_timezone_set'))
 
 ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . '/Users/keithpalmerjr/Projects/QuickBooks');
 
-/**
- * 
- */
 require_once 'QuickBooks.php';
 
 //$user = 'api';
@@ -42,94 +39,6 @@ if (!QuickBooks_Utilities::initialized($api_driver_dsn))
 $API = new QuickBooks_API($api_driver_dsn, $user, $source_type, $source_dsn, $api_options, $source_options, $driver_options);
 
 
-// CUSTOMERS
-/*
-$fname = 'Brayan ' . mt_rand();
-$lname = 'Taylor';
-
-$Customer = new QuickBooks_Object_Customer();
-
-$Customer->setFirstName($fname);
-$Customer->setLastName($lname);
-
-$Customer->setShipAddress('56 Cowles Road', '', '', '', '', 'Willington', 'CT');
-$Customer->setMiddleName('R');
-$Customer->setSalutation('Mr.');
-
-$Customer->setPhone('1.860.634.1602');
-
-$API->addCustomer($Customer, '_quickbooks_customer_add_callback', 15);
-*/
-/*
-
-// INVOICES
-$Invoice = new QuickBooks_Object_Invoice();
-//$Invoice->setOther('test of other');		// for some reason this field doesn't work...
-$Invoice->setMemo('test of a memo');
-$Invoice->setCustomerApplicationID(15);
-$Invoice->setRefNumber(125);
-
-$InvoiceLine1 = new QuickBooks_Object_Invoice_InvoiceLine();
-$InvoiceLine1->setItemApplicationID(12);
-$InvoiceLine1->setAmount(300.00);
-$InvoiceLine1->setQuantity(3);
-
-$InvoiceLine2 = new QuickBooks_Object_Invoice_InvoiceLine();
-$InvoiceLine2->setItemApplicationID(11);
-$InvoiceLine2->setAmount(225.00);
-$InvoiceLine2->setQuantity(5);
-
-$Invoice->addInvoiceLine($InvoiceLine1);
-$Invoice->addInvoiceLine($InvoiceLine2);
-
-$API->addInvoice($Invoice, '_quickbooks_invoice_add_callback', 20);
-
-
-// VENDORS
-$Vendor = new QuickBooks_Object_Vendor();
-$Vendor->setName('Test Vendor ' . mt_rand());
-$Vendor->setPhone('1.860.634.1602');
-$Vendor->setFirstName('Test');
-$Vendor->setFax('1.860.429.5183');
-
-$API->addVendor($Vendor, '_quickbooks_vendor_add_callback', 19);
-
-
-// SERVICE ITEMS
-$ServiceItem = new QuickBooks_Object_ServiceItem();
-$ServiceItem->isSalesOrPurchase(true);
-
-$ServiceItem->setName('My Service Item ' . mt_rand());
-$ServiceItem->setPrice(250);
-$ServiceItem->setDescription('My Test Item Description');
-$ServiceItem->setAccountName('Sales');
-
-
-$API->addServiceItem($ServiceItem, '_quickbooks_serviceitem_add_callback', 12);
-
-
-// INVENTORY ITEMS
-$InventoryItem = new QuickBooks_Object_InventoryItem();
-$InventoryItem->setName('Test Inventory Item ' . mt_rand());
-$InventoryItem->setSalesPrice(25.25);
-
-$InventoryItem->setIncomeAccountName('Sales');
-$InventoryItem->setCOGSAccountName('Cost of Goods Sold');
-$InventoryItem->setAssetAccountName('Inventory Asset');
-$InventoryItem->setPreferredVendorApplicationID(19);
-
-$API->addInventoryItem($InventoryItem, '_quickbooks_inventoryitem_add_callback', 11);
-*/
-
-/*
-// QUERYING FOR ACCOUNTS
-$datetime = '2009-01-02 01:02:03';
-$API->listAccountsModifiedAfter($datetime, '_quickbooks_account_query_callback');
-*/
-
-//$datetime = '2009-01-02 01:02:03';
-//$API->listAccountsModifiedAfter($datetime, '_quickbooks_account_query_callback');
-
 $dac = $REQUEST["dac"];
 $dam = $REQUEST["dam"];
 $cac = $REQUEST["cac"];
@@ -140,21 +49,23 @@ $cam = $REQUEST["cam"];
 // Journal entry
 $JournalEntry = new QuickBooks_Object_JournalEntry();
 
-$JournalEntry->setTransactionDate('July 23, 2012');
+$dDate = date("F d, Y");
+//$JournalEntry->setTransactionDate('July 23, 2012');
+$JournalEntry->setTransactionDate($dDate);
 
 $DebitLine = new QuickBooks_Object_JournalEntry_JournalDebitLine();
-$DebitLine->setAmount(45.0);
 //$DebitLine->setAccountName('Travel Expense');
 //$JournalEntry->addDebitLine($DebitLine);
 $DebitLine->setAccountName($dac);
-$JournalEntry->addDebitLine($dam);
+$DebitLine->setAmount($dam);
+$JournalEntry->addDebitLine($DebitLine);
 
 $CreditLine = new QuickBooks_Object_JournalEntry_JournalCreditLine();
-$CreditLine->setAmount(45.0);
 //$CreditLine->setAccountName('Bank Account XYZ');
 //$JournalEntry->addCreditLine($CreditLine);
 $CreditLine->setAccountName($cac);
-$JournalEntry->addCreditLine($cam);
+$CreditLine->setAmount($cam);
+$JournalEntry->addCreditLine($CreditLine);
 
 if ($API->addJournalEntry(
 	$JournalEntry, 
